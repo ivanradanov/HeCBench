ca02714c0cc3e929150dff8aaa5a466a96089ab7
Ivan Radanov Ivanov <ivanov2@llnl.gov>
2023-11-11 12:56:08 -0800

    Run info output

--- src/scripts/flux_job.sh
+++ src/scripts/flux_job.sh
@@ -7,6 +7,11 @@ i="$1"
 dc="$2"
 OMP_PROFILE_DIR="$3"
 
+RUN_INFO="$OMP_PROFILE_DIR/run_info"
+
+echo "i is $i" &>> "$RUN_INFO"
+echo "dc is $dc" &>> "$RUN_INFO"
+
 CURDATE=$(date +"%Y-%m-%dT%H:%M:%S%z")
 HOST=$(hostname)
 
@@ -22,7 +27,9 @@ git clone --depth=1 file://"$ORIGINAL_HECDIR" "$HECDIR"
 #cp -a "$HOME/src/HeCBench" "$HECDIR"
 
 cd "$HECDIR"
-#git restore .
+
+echo "HecBench:" &>> "$RUN_INFO"
+git show &>> "$RUN_INFO"
 
 make omp_clean -j 90 -k || true
 
@@ -33,8 +40,12 @@ ls -lat
 llvm-build-corona.sh --release
 source "$HOME/bin/llvm-enable-corona.sh" --release
 
+echo "clang:" &>> "$RUN_INFO"
 command -v clang
 command -v clang++
+
+clang --version &>> "$RUN_INFO"
+clang++ --version &>> "$RUN_INFO"
 
 export UNROLL_AND_INTERLEAVE_FACTOR="$i"
 export UNROLL_AND_INTERLEAVE_DYNAMIC_CONVERGENCE="$dc"
